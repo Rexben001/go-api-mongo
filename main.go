2967d1476013ab9d77ad39981d3d94f13e75ab57
Rexben <rexben.rb@gmail.com>
2020-08-25 23:52:57 +0100

    create main func

--- main.go
+++ main.go
@@ -21,3 +21,22 @@ type Person struct {
 	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
 }
 
+func main() {
+	fmt.Println("App has started!!!!")
+	// define timeout for Mongo and Go
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// mongodb connection
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if client != nil {
+		fmt.Println("Connected successfully")
+
+	}
+	// define router
+	router := mux.NewRouter()
+	router.HandleFunc("/person", AddPerson).Methods("POST")
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/person/{id}", GetPerson).Methods("GET")
+	http.ListenAndServe(":12345", router)
+}
