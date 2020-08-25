f120039313b416154505fc2ab79ea7a34379b0c3
Rexben <rexben.rb@gmail.com>
2020-08-25 23:52:36 +0100

    create person struct

--- main.go
+++ main.go
@@ -14,3 +14,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
