4a9bf1630e25e521d1f651572e9e12825cb606fb
Rexben <rexben.rb@gmail.com>
2020-08-25 23:52:16 +0100

    import dependencies

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
