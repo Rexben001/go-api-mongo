e29f91167d671eae86dde6f28e714329ae216dfd
Rexben <rexben.rb@gmail.com>
2020-08-26 21:09:10 +0100

    delete person

--- main.go
+++ main.go
@@ -93,6 +93,33 @@ func GetPerson(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode(person)
 }
 
+func DeletePerson(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	// get the params from the requst
+	params := mux.Vars(request)
+	// convert params id (string) to MongoDB ID
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	collection := client.Database("peoplerex").Collection("people")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// get item by id
+	result, err := collection.DeleteOne(ctx, bson.M{"_id": id})
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+	if result.DeletedCount == 0 {
+		// log.Fatal("Error on deleting one Hero", err)
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "Unable to delete item"}`))
+		return
+	}
+	response.WriteHeader(http.StatusNoContent)
+	json.NewEncoder(response).Encode(result.DeletedCount)
+}
+
 func main() {
 	fmt.Println("App has started!!!!")
 	// define timeout for Mongo and Go
@@ -110,5 +137,6 @@ func main() {
 	router.HandleFunc("/person", AddPerson).Methods("POST")
 	router.HandleFunc("/people", GetPeople).Methods("GET")
 	router.HandleFunc("/person/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/person/{id}", DeletePerson).Methods("DELETE")
 	http.ListenAndServe(":12345", router)
 }
