060e5c65b11680f97ff14ca05172604bca4b7e84
Rexben <rexben.rb@gmail.com>
2020-08-26 21:28:57 +0100

    handle error in AddPerson

--- main.go
+++ main.go
@@ -34,7 +34,11 @@ func AddPerson(response http.ResponseWriter, request *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	result, _ := collection.InsertOne(ctx, person)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
 	json.NewEncoder(response).Encode(result)
 }
 
