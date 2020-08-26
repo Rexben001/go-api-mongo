0c792e7b9416e98c2881e3314d55a3b98af295a5
Rexben <rexben.rb@gmail.com>
2020-08-26 23:42:13 +0100

    return a well defined response

--- addPerson.go
+++ addPerson.go
@@ -6,8 +6,15 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type InsertOneResult struct {
+	// The identifier that was inserted.
+	InsertedID interface{}
+}
+type ObjectID [12]byte
 
 func AddPerson(response http.ResponseWriter, request *http.Request) {
 
@@ -30,6 +37,19 @@ func AddPerson(response http.ResponseWriter, request *http.Request) {
 		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
 		return
 	}
+
+	var id string
+
+	if oid, ok := result.InsertedID.(primitive.ObjectID); ok {
+		id = oid.Hex()
+	}
+	finalResult := make(map[string]string)
+
+	finalResult["message"] = "New person added successfully"
+	finalResult["InsertedId"] = id
+	finalResult["status"] = "200"
+	finalResult["success"] = "True"
+
 	// writes the objects to standard output
-	json.NewEncoder(response).Encode(result)
+	json.NewEncoder(response).Encode(finalResult)
 }
